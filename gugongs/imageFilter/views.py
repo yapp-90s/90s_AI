925e10709987298cd1423c07a2445f617680c00f
FCJYCT <radiata03@gmail.com>
2022-02-19 17:13:54 +0900

    ..

--- gugongs/imageFilter/views.py
+++ gugongs/imageFilter/views.py
@@ -17,15 +17,18 @@ def apply(request):
 
 
     data = JSONParser().parse(request)
+
     film_code = data['film_code']
-    film_uid = data['film_uid']
-    photo_uid = data['photo_uid']
+    s3_file_path = data['s3_file_path']
+    before_file_name = data['before_file_name']
+    after_file_name = data['after_file_name']
+    
 
     #사진 저장 경로
     current_dir = os.getcwd()
     save_dir = os.path.join(current_dir, 'imageFilter', 'images')  # current/imageFilter/images
-    before_img = save_dir + '/%d.jpeg' % photo_uid
-    after_img = save_dir + '/%d_edited.jpeg' % photo_uid
+    before_img = save_dir + '/' + before_file_name
+    after_img = save_dir + '/' + after_file_name
 
 
 
@@ -36,10 +39,9 @@ def apply(request):
 
     # 버킷 이름 / 다운로드 할 객체 지정 / 다운로드할 위치와 파일명
     s3.download_file(AWS_STORAGE_BUCKET_NAME,
-                        '%d/%d.jpeg' % (film_uid, photo_uid),
+                        '%s%s' % (s3_file_path, before_file_name),
                         before_img)
     
-    print("5555")
 
     # 필터 적용
     image = cv2.imread(before_img, 1)  # 1 color, 2 grayscale, -1 alpha channel 포함
@@ -58,13 +60,8 @@ def apply(request):
     
 
     # 업로드 할 파일 / 버킷 이름 / 업로드될 객체
-    s3.copy_object(
-        Bucket=AWS_STORAGE_BUCKET_NAME, 
-        CopySource={'Bucket' : AWS_STORAGE_BUCKET_NAME, 'Key': '%d/%d.jpeg' % (film_uid, photo_uid)},
-        Key='%d/%d_orgin.jpeg' % (film_uid, photo_uid))
-    s3.delete_object(Bucket=AWS_STORAGE_BUCKET_NAME, Key='%d/%d.jpeg' % (film_uid, photo_uid))
-    
-    s3.upload_file(after_img,AWS_STORAGE_BUCKET_NAME, '%d/%d.jpeg' % (film_uid, photo_uid))
+
+    s3.upload_file(after_img,AWS_STORAGE_BUCKET_NAME, '%s%s' % (s3_file_path, after_file_name))
 
     # 처리 끝난 이미지 프로젝트에서 삭제
     if os.path.exists(before_img):
